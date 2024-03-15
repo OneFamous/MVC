56a4332643213909c285bf92ba06c37908c3715c
THEPHATIH\fatih <fatih.atese@yahoo.com>
2024-03-15 15:25:59 +0300

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using deneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deneme.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "IUC BİLGİSAYAR" };
+            return View(movie);
+        }
+        public ActionResult Edit(int id) 
+        {
+            return Content("id=" + id);
+        
+        }
+        public ActionResult ByReleasedate(int year, int month) 
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
